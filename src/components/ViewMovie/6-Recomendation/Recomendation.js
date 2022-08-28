40302b2c8f47df140626f8a49522fa9389c0e6cd
Facundo Alemandi <facuinstituto10@gmail.com>
2022-08-28 02:27:00 -0300

    Arreglando una coas en recomendaciones

--- src/components/ViewMovie/6-Recomendation/Recomendation.js
+++ src/components/ViewMovie/6-Recomendation/Recomendation.js
@@ -122,7 +122,7 @@ const Recomendations = () => {
         <H3>Recomendations</H3>
         <SectionMovieCredits>
           {newData.map((el) => (
-            <NavL to={`/movie/${el.id}/${el.title}`}>
+            <NavL to={`/${watch}/${el.id}/${el.title}`}>
               <Img
                   alt={el.original_title}
                   src={
